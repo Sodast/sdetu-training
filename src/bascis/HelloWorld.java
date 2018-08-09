db9fd8ca9b7fbbd7c69c06ccb11d11c29da4158d
Chris <cdick88@hotmail.com>
2018-08-09 12:10:25 +0200

    Medified Hello world app

--- src/bascis/HelloWorld.java
+++ src/bascis/HelloWorld.java
@@ -3,7 +3,8 @@ package bascis;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		System.out.println("I'm adding code to my project");
 
 	}
 
